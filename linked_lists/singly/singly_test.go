4fdfd90410e0c0faf82baa139bc0f9e385467d48
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-11 13:13:26 +0200

    test: add test that cover the add method

---
+++ linked_lists/singly/singly_test.go
@@ -0,0 +1,52 @@
+package singly
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		list
+		size      int
+		lastValue int
+	}{
+		{list: fillList(), size: 0, lastValue: 0},
+		{list: fillList(10), size: 1, lastValue: 10},
+		{list: fillList(10, 20, 30), size: 3, lastValue: 30},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("Test number: %d", i+1), func(t *testing.T) {
+			if tt.size != tt.list.size {
+				t.Errorf("Expected size: %d got %d", tt.size, tt.list.size)
+			}
+
+			if lv := getLastValList(tt.list); tt.lastValue != lv {
+				t.Errorf("Expected value: %d got %d", tt.lastValue, lv)
+			}
+		})
+	}
+}
+
+func fillList(values ...int) list {
+	l := list{}
+	for _, v := range values {
+		l.add(v)
+	}
+	return l
+}
+
+func getLastValList(l list) int {
+	if l.head == nil {
+		return 0
+	}
+
+	var r int
+	for i, curr := 0, l.head; i < l.size; i++ {
+		r = curr.value
+		curr = curr.next
+	}
+
+	return r
+}
