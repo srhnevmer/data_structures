0f7f5b39bf4c90d060d28e4a20803c8eabde19ec
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-11 15:31:20 +0200

    test: add the test that covers the method for traversal list

--- linked_lists/singly/singly_test.go
+++ linked_lists/singly/singly_test.go
@@ -2,8 +2,11 @@ package singly
 
 import (
 	"fmt"
+	"slices"
 	"testing"
 )
+
+const template = "Test case number:"
 
 func TestAdd(t *testing.T) {
 	testCases := []struct {
@@ -49,4 +52,23 @@ func getLastValList(l list) int {
 	}
 
 	return r
+}
+
+func TestTraversal(t *testing.T) {
+	testCases := []struct {
+		list
+		want []int
+	}{
+		{list: fillList(), want: make([]int, 0)},
+		{list: fillList(10), want: []int{10}},
+		{list: fillList(10, 20, 30), want: []int{10, 20, 30}},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%s%d", template, i+1), func(t *testing.T) {
+			if got := tc.list.traversal(); slices.Compare(tc.want, got) != 0 {
+				t.Errorf("Expected result: %v got %v", tc.want, got)
+			}
+		})
+	}
 }
