b79f165eda99a3c83f513738d743c2d37a219a46
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-11 15:43:01 +0200

    feat: add the method for traversal the list

--- linked_lists/singly/singly.go
+++ linked_lists/singly/singly.go
@@ -25,3 +25,16 @@ func (l *list) add(val int) {
 		curr.next = n
 	}
 }
+
+func (l *list) traversal() []int {
+	if l.head == nil {
+		return make([]int, 0)
+	}
+
+	r := make([]int, 0, l.size)
+	for curr := l.head; curr != nil; curr = curr.next {
+		r = append(r, curr.value)
+	}
+
+	return r
+}
