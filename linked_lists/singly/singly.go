24845c6cf68bdaa0d181c083bb85a667e9bb0130
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-12 15:04:32 +0200

    feat: add the insert method

--- linked_lists/singly/singly.go
+++ linked_lists/singly/singly.go
@@ -10,15 +10,46 @@ type list struct {
 	size int
 }
 
-func (l *list) traversal() []int {
-	if l.head == nil {
-		return make([]int, 0)
+func (l *list) insert(idx, val int) {
+	if idx < 0 || l.size < idx {
+		return
 	}
 
-	r := make([]int, 0, l.size)
-	for curr := l.head; curr != nil; curr = curr.next {
-		r = append(r, curr.value)
+	l.size++
+	n := &node{nil, val}
+
+	switch {
+	case l.head == nil:
+		l.head = n
+	case idx == 0:
+		n.next = l.head
+		l.head = n
+	default:
+		curr := l.head
+		for prevIdx := 0; prevIdx < idx-1; prevIdx++ {
+			curr = curr.next
+		}
+
+		if curr.next == nil {
+			curr.next = n
+			return
+		}
+
+		n.next = curr.next
+		curr.next = n
 	}
 
-	return r
 }
+
+// func (l *list) traversal() []int {
+// 	if l.head == nil {
+// 		return make([]int, 0)
+// 	}
+
+// 	r := make([]int, 0, l.size)
+// 	for curr := l.head; curr != nil; curr = curr.next {
+// 		r = append(r, curr.value)
+// 	}
+
+// 	return r
+// }
