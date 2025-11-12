ea514e5220384b6e79cd338dfb31eb5c0f4c1383
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-12 14:31:23 +0200

    feat: delete the old code base

--- linked_lists/singly/singly_test.go
+++ linked_lists/singly/singly_test.go
@@ -1,22 +1,3 @@
 package singly
 
 const template = "Test case number:"
-
-// func TestTraversal(t *testing.T) {
-// 	testCases := []struct {
-// 		list
-// 		want []int
-// 	}{
-// 		{list: fillList(), want: make([]int, 0)},
-// 		{list: fillList(10), want: []int{10}},
-// 		{list: fillList(10, 20, 30), want: []int{10, 20, 30}},
-// 	}
-
-// 	for i, tc := range testCases {
-// 		t.Run(fmt.Sprintf("%s%d", template, i+1), func(t *testing.T) {
-// 			if got := tc.list.traversal(); slices.Compare(tc.want, got) != 0 {
-// 				t.Errorf("Expected result: %v got %v", tc.want, got)
-// 			}
-// 		})
-// 	}
-// }
