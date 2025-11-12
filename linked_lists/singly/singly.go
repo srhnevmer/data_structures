9427b7f189b5b6d59eff3f47c461197bce18a0c8
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-12 15:07:40 +0200

    feat: delete the old version of the method traverse

--- linked_lists/singly/singly.go
+++ linked_lists/singly/singly.go
@@ -40,16 +40,3 @@ func (l *list) insert(idx, val int) {
 	}
 
 }
-
-// func (l *list) traversal() []int {
-// 	if l.head == nil {
-// 		return make([]int, 0)
-// 	}
-
-// 	r := make([]int, 0, l.size)
-// 	for curr := l.head; curr != nil; curr = curr.next {
-// 		r = append(r, curr.value)
-// 	}
-
-// 	return r
-// }
