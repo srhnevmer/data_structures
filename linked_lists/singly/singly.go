44c42bd86d90d1b77593f1414ad5ac33fb565928
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-13 16:26:45 +0200

    feat: add the delete method

--- linked_lists/singly/singly.go
+++ linked_lists/singly/singly.go
@@ -40,6 +40,37 @@ func (l *list) insert(idx uint, val int) {
 	}
 }
 
+func (l *list) delete(idx uint) {
+	if l.size == 0 || l.size <= idx {
+		return
+	}
+
+	l.size--
+
+	switch {
+	case l.size == 1:
+		l.head = nil
+	case idx == 0:
+		target := l.head
+		l.head = target.next
+		target.next = nil
+	default:
+		curr := l.head
+		for i := uint(0); i < idx-1; i++ {
+			curr = curr.next
+		}
+
+		target := curr.next
+		if target == nil {
+			curr.next = nil
+			return
+		}
+
+		curr.next = target.next
+		target.next = nil
+	}
+}
+
 func (l *list) traverse() []int {
 	r := make([]int, 0, l.size)
 	for curr := l.head; curr != nil; curr = curr.next {
