6465668495df8bf1a4ae2c30b2b725b02dd285f5
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-16 12:07:08 +0200

    test: refactor the code base

--- linked_lists/singly/singly_test.go
+++ linked_lists/singly/singly_test.go
@@ -20,10 +20,10 @@ func TestInsert(t *testing.T) {
 		size  uint
 		want  []int
 	}{
-		{list: list{}, index: 1, value: val, size: 0, want: []int{}},                       // empty list invalid index
-		{list: list{}, index: 0, value: val, size: 1, want: []int{50}},                     // empty list valid index
-		{list: fillList(nums), index: 4, value: val, size: 3, want: []int{10, 20, 30}},     // filled list invalid index
-		{list: fillList(nums), index: 0, value: val, size: 4, want: []int{50, 10, 20, 30}}, // filled list valid index
+		{list: list{}, index: 1, value: val, size: 0, want: []int{}},
+		{list: list{}, index: 0, value: val, size: 1, want: []int{50}},
+		{list: fillList(nums), index: 4, value: val, size: 3, want: []int{10, 20, 30}},
+		{list: fillList(nums), index: 0, value: val, size: 4, want: []int{50, 10, 20, 30}},
 		{list: fillList(nums), index: 1, value: val, size: 4, want: []int{10, 50, 20, 30}},
 		{list: fillList(nums), index: 2, value: val, size: 4, want: []int{10, 20, 50, 30}},
 		{list: fillList(nums), index: 3, value: val, size: 4, want: []int{10, 20, 30, 50}},
@@ -71,14 +71,14 @@ func TestDelete(t *testing.T) {
 		size  uint
 		want  []int
 	}{
-		{list: list{}, index: 0, size: 0, want: []int{}},               // empty list valid index
-		{list: list{}, index: 1, size: 0, want: []int{}},               // empty list invalid index
-		{list: fillList(nums[:1]), index: 0, size: 0, want: []int{}},   // filled list valid index
-		{list: fillList(nums[:1]), index: 1, size: 1, want: []int{10}}, // filled list invalid index
+		{list: list{}, index: 1, size: 0, want: []int{}},
+		{list: list{}, index: 0, size: 0, want: []int{}},
+		{list: fillList(nums[:1]), index: 1, size: 1, want: []int{10}},
+		{list: fillList(nums[:1]), index: 0, size: 0, want: []int{}},
 		{list: fillList(nums), index: 0, size: 2, want: []int{20, 30}},
 		{list: fillList(nums), index: 1, size: 2, want: []int{10, 30}},
 		{list: fillList(nums), index: 2, size: 2, want: []int{10, 20}},
-		{list: fillList(nums), index: 3, size: 3, want: []int{10, 20, 30}}, // filled list invalid index
+		{list: fillList(nums), index: 3, size: 3, want: []int{10, 20, 30}},
 	}
 
 	for i, tc := range testCases {
@@ -122,7 +122,6 @@ func TestSearch(t *testing.T) {
 		want  bool
 	}{
 		{list: list{}, value: val, want: false},
-		{list: fillList(nums[:1]), value: val, want: false},
 		{list: fillList(nums), value: 10, want: true},
 		{list: fillList(nums), value: 20, want: true},
 		{list: fillList(nums), value: 30, want: true},
