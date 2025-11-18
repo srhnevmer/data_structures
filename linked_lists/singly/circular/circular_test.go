0614407e22b276baf2311f273c2b3959f3c1c317
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-18 11:28:11 +0200

    test: rewrite test cases that cover the insert method

--- linked_lists/singly/circular/circular_test.go
+++ linked_lists/singly/circular/circular_test.go
@@ -11,99 +11,94 @@ var (
 )
 
 func TestInsert(t *testing.T) {
-	t.Run("Insert in empty list with invalid index", func(t *testing.T) {
-		l := list{}
-		l.insert(1, val)
-		assertSize(t, uint(0), l.size)
-		assertValues(t, []int{}, getValues(l))
-		if got := l.head; got != nil {
-			t.Errorf("Expected result: %v, got: %p", nil, got)
-		}
-	})
-	t.Run("Insert in empty list with valid index", func(t *testing.T) {
-		l := list{}
-		l.insert(0, val)
-		assertSize(t, uint(1), l.size)
-		assertValues(t, []int{val}, getValues(l))
-		assertPointer(t, l.head, getPtrFromLastNode(l))
-	})
-	t.Run("Insert into the filled list at the zero index", func(t *testing.T) {
-		l := fillList(values)
-		l.insert(0, val)
-		assertSize(t, uint(4), l.size)
-		assertValues(t, []int{val, 10, 20, 30}, getValues(l))
-		assertPointer(t, l.head, getPtrFromLastNode(l))
-	})
-	t.Run("Insert into the filled list at the first index", func(t *testing.T) {
-		l := fillList(values)
-		l.insert(1, val)
-		assertSize(t, uint(4), l.size)
-		assertValues(t, []int{10, val, 20, 30}, getValues(l))
-		assertPointer(t, l.head, getPtrFromLastNode(l))
-	})
-	t.Run("Insert into the filled list at the second index", func(t *testing.T) {
-		l := fillList(values)
-		l.insert(2, val)
-		assertSize(t, uint(4), l.size)
-		assertValues(t, []int{10, 20, val, 30}, getValues(l))
-		assertPointer(t, l.head, getPtrFromLastNode(l))
-	})
-	t.Run("Insert into the filled list at the end of the list", func(t *testing.T) {
-		l := fillList(values)
-		l.insert(3, val)
-		assertSize(t, uint(4), l.size)
-		assertValues(t, []int{10, 20, 30, val}, getValues(l))
-		assertPointer(t, l.head, getPtrFromLastNode(l))
-	})
-	t.Run("Insert into the filled list with invalid index", func(t *testing.T) {
-		l := fillList(values)
-		l.insert(4, val)
-		assertSize(t, uint(3), l.size)
-		assertValues(t, values, getValues(l))
-		assertPointer(t, l.head, getPtrFromLastNode(l))
-	})
-}
+	testCases := []struct {
+		name string
+		list
+		size   uint
+		index  uint
+		value  int
+		values []int
+	}{
+		{
+			name:   "Insert into an empty list with an invalid index",
+			list:   list{},
+			size:   0,
+			index:  1,
+			value:  val,
+			values: []int{},
+		},
+		{
+			name:   "Insert into an empty list with a valid index",
+			list:   list{},
+			size:   1,
+			index:  0,
+			value:  val,
+			values: []int{val},
+		},
+		{
+			name:   "Insert into the filled list with an invalid index",
+			list:   getFilledList(),
+			size:   3,
+			index:  4,
+			value:  val,
+			values: values,
+		},
+		{
+			name:   "Insert into the filled list at the zero index",
+			list:   getFilledList(),
+			size:   4,
+			index:  0,
+			value:  val,
+			values: []int{val, 10, 20, 30},
+		},
+		{
+			name:   "Insert into the filled list at the first index",
+			list:   getFilledList(),
+			size:   4,
+			index:  1,
+			value:  val,
+			values: []int{10, val, 20, 30},
+		},
+		{
+			name:   "Insert into the filled list at the second index",
+			list:   getFilledList(),
+			size:   4,
+			index:  2,
+			value:  val,
+			values: []int{10, 20, val, 30},
+		},
+		{
+			name:   "Insert into the filled list at the end of the list",
+			list:   getFilledList(),
+			size:   4,
+			index:  3,
+			value:  val,
+			values: []int{10, 20, 30, 40},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.list.insert(tc.index, tc.value)
+			if want, got := tc.size, tc.list.size; want != got {
+				t.Fatalf("Expected size: %d got: %d", want, got)
+			}
 
-func assertSize(t testing.TB, want, got uint) {
-	t.Helper()
-	if want != got {
-		t.Fatalf("Expected size: %d got: %d", want, got)
+			if want, got := tc.values, []int{}; slices.Compare(want, got) != 0 {
+				t.Fatalf("Expected values: %v got: %v", want, got)
+			}
+
+			if want, got := tc.list.head, tc.list.tail.next; want != got {
+				t.Errorf("Expected result: [ptr: %p value: %[1]v] got: [ptr: %p value: %[2]v]", want, got)
+			}
+		})
 	}
 }
 
-func assertValues(t testing.TB, want, got []int) {
-	t.Helper()
-	if slices.Compare(want, got) != 0 {
-		t.Fatalf("Expected result: %v got: %v", want, got)
-	}
-}
-
-func assertPointer(t testing.TB, want, got *node) {
-	if want != got {
-		t.Errorf("Expected result: [ptr: %p val: %[1]v] got: [ptr: %p val: %[2]v]", want, got)
-	}
-}
-
-func getValues(l list) []int {
-	res := make([]int, 0, l.size)
-	for i, curr := uint(0), l.head; i < l.size; i, curr = i+1, curr.next {
-		res = append(res, curr.value)
-	}
-	return res
-}
-
-func getPtrFromLastNode(l list) *node {
-	curr := l.head
-	for i := uint(1); i < l.size; i++ {
-		curr = curr.next
-	}
-	return curr.next
-}
-
-func fillList(values []int) list {
+func getFilledList() list {
 	l := list{}
 	for i := range values {
 		l.insert(uint(i), values[i])
 	}
 	return l
 }
+x
