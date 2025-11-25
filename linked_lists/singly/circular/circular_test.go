6979d8e5cbb0bb3124c86a1067e274d1acd61d3c
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-11-25 11:46:51 +0200

    test: rewrite the way of  implementing test cases

--- linked_lists/singly/circular/circular_test.go
+++ linked_lists/singly/circular/circular_test.go
@@ -6,32 +6,75 @@ import (
 	"testing"
 )
 
-var (
-	value        = 50
-	values       = []int{10, 20, 30}
-	invalidIndex = uint(10)
-)
-
 func TestInsert(t *testing.T) {
 	testCases := []struct {
 		name string
 		list
 		size           uint
 		index          uint
-		payload        int
+		value          int
 		expectedValues []int
 	}{
-		{name: "Attempt to insert into an empty list with an invalid index", list: list{}, size: 0, index: invalidIndex, payload: value, expectedValues: []int{}},
-		{name: "Insert into an empty list with a valid index", list: list{}, size: 1, index: 0, payload: value, expectedValues: []int{50}},
-		{name: "Attempt to insert into a filled list with an invalid index", list: getFilledList(values), size: 3, index: invalidIndex, payload: value, expectedValues: []int{10, 20, 30}},
-		{name: "Insert into a filled list at the zero index", list: getFilledList(values), size: 4, index: 0, payload: value, expectedValues: []int{value, 10, 20, 30}},
-		{name: "Insert into a filled list at the first index", list: getFilledList(values), size: 4, index: 1, payload: value, expectedValues: []int{10, value, 20, 30}},
-		{name: "Insert into a filled list at the second index", list: getFilledList(values), size: 4, index: 2, payload: value, expectedValues: []int{10, 20, value, 30}},
-		{name: "Insert into a filled list at the end of the list", list: getFilledList(values), size: 4, index: 3, payload: value, expectedValues: []int{10, 20, 30, value}},
+		{
+			name:           "Attempt to insert into an empty list with an invalid index",
+			list:           list{},
+			size:           0,
+			index:          1,
+			value:          50,
+			expectedValues: []int{},
+		},
+		{
+			name:           "Insert into an empty list with a valid index",
+			list:           list{},
+			size:           1,
+			index:          0,
+			value:          50,
+			expectedValues: []int{50},
+		},
+		{
+			name:           "Attempt to insert into a filled list with an invalid index",
+			list:           getFilledList([]int{10, 20, 30}),
+			size:           3,
+			index:          4,
+			value:          50,
+			expectedValues: []int{10, 20, 30},
+		},
+		{
+			name:           "Insert into a filled list at the zero index",
+			list:           getFilledList([]int{10, 20, 30}),
+			size:           4,
+			index:          0,
+			value:          50,
+			expectedValues: []int{50, 10, 20, 30},
+		},
+		{
+			name:           "Insert into a filled list at the first index",
+			list:           getFilledList([]int{10, 20, 30}),
+			size:           4,
+			index:          1,
+			value:          50,
+			expectedValues: []int{10, 50, 20, 30},
+		},
+		{
+			name:           "Insert into a filled list at the second index",
+			list:           getFilledList([]int{10, 20, 30}),
+			size:           4,
+			index:          2,
+			value:          50,
+			expectedValues: []int{10, 20, 50, 30},
+		},
+		{
+			name:           "Insert into a filled list at the end of the list",
+			list:           getFilledList([]int{10, 20, 30}),
+			size:           4,
+			index:          3,
+			value:          50,
+			expectedValues: []int{10, 20, 30, 50},
+		},
 	}
 	for i, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.list.insert(tc.index, tc.payload)
+			tc.list.insert(tc.index, tc.value)
 			assertSize(t, tc.size, tc.list.size)
 			assertValues(t, tc.expectedValues, getValuesFromList(tc.list))
 			if i != 0 {
@@ -49,13 +92,55 @@ func TestDelete(t *testing.T) {
 		index          uint
 		expectedValues []int
 	}{
-		{name: "Attempt to delete a value from an empty list", list: list{}, size: 0, index: 0, expectedValues: []int{}},
-		{name: "Delete a value from a list of size 1 with a valid index", list: getFilledList(values[:1]), size: 0, index: 0, expectedValues: []int{}},
-		{name: "Attempt to delete a value from a list of size 1 with an invalid index", list: getFilledList(values[:1]), size: 1, index: invalidIndex, expectedValues: []int{10}},
-		{name: "Attempt to delete a value from a filled list with an invalid index", list: getFilledList(values), size: 3, index: invalidIndex, expectedValues: []int{10, 20, 30}},
-		{name: "Delete a value from a filled list at the zero index", list: getFilledList(values), size: 2, index: 0, expectedValues: []int{20, 30}},
-		{name: "Delete a value from a filled list at the first index", list: getFilledList(values), size: 2, index: 1, expectedValues: []int{10, 30}},
-		{name: "Delete a value from a filled list at the second index", list: getFilledList(values), size: 2, index: 2, expectedValues: []int{10, 20}},
+		{
+			name:           "Attempt to delete a value from an empty list",
+			list:           list{},
+			size:           0,
+			index:          0,
+			expectedValues: []int{},
+		},
+		{
+			name:           "Delete a value from a list of size 1 with a valid index",
+			list:           getFilledList([]int{10}),
+			size:           0,
+			index:          0,
+			expectedValues: []int{},
+		},
+		{
+			name:           "Attempt to delete a value from a list of size 1 with an invalid index",
+			list:           getFilledList([]int{10}),
+			size:           1,
+			index:          1,
+			expectedValues: []int{10},
+		},
+		{
+			name:           "Attempt to delete a value from a filled list with an invalid index",
+			list:           getFilledList([]int{10, 20, 30}),
+			size:           3,
+			index:          3,
+			expectedValues: []int{10, 20, 30},
+		},
+		{
+			name:           "Delete a value from a filled list at the zero index",
+			list:           getFilledList([]int{10, 20, 30}),
+			size:           2,
+			index:          0,
+			expectedValues: []int{20, 30},
+		},
+		{
+			name:           "Delete a value from a filled list at the first index",
+			list:           getFilledList([]int{10, 20, 30}),
+			size:           2,
+			index:          1,
+			expectedValues: []int{10, 30},
+		},
+		{
+			name:           "Delete a value from a filled list at the second index",
+			list:           getFilledList([]int{10, 20, 30}),
+			size:           2,
+			index:          2,
+			expectedValues: []int{10, 20},
+		},
 	}
 	for i, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -77,11 +162,41 @@ func TestSearch(t *testing.T) {
 		expectedIndex  uint
 		expectedResult bool
 	}{
-		{name: "Attempt to find a value in an empty list", list: list{}, target: value, expectedIndex: 0, expectedResult: false},
-		{name: "Attempt to find a value in a filled list but a value doesn't exist", target: value, list: getFilledList(values), expectedIndex: 0, expectedResult: false},
-		{name: "To find a value that is equal to 10", list: getFilledList(values), target: 10, expectedIndex: 0, expectedResult: true},
-		{name: "To find a value that is equal to 20", list: getFilledList(values), target: 20, expectedIndex: 1, expectedResult: true},
-		{name: "To find a value that is equal to 30", list: getFilledList(values), target: 30, expectedIndex: 2, expectedResult: true},
+		{
+			name:           "Attempt to find a value in an empty list",
+			list:           list{},
+			target:         50,
+			expectedIndex:  0,
+			expectedResult: false,
+		},
+		{
+			name:           "Attempt to find a value in a filled list but a value doesn't exist",
+			target:         50,
+			list:           getFilledList([]int{10, 20, 30}),
+			expectedIndex:  0,
+			expectedResult: false,
+		},
+		{
+			name:           "Find the value that equals 10",
+			list:           getFilledList([]int{10, 20, 30}),
+			target:         10,
+			expectedIndex:  0,
+			expectedResult: true,
+		},
+		{
+			name:           "Find the value that equals 20",
+			list:           getFilledList([]int{10, 20, 30}),
+			target:         20,
+			expectedIndex:  1,
+			expectedResult: true,
+		},
+		{
+			name:           "Find the value that equals 30",
+			list:           getFilledList([]int{10, 20, 30}),
+			target:         30,
+			expectedIndex:  2,
+			expectedResult: true,
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -100,16 +215,28 @@ func TestReverse(t *testing.T) {
 	testCases := []struct {
 		name string
 		list
-		expectedList list
+		expectedValues []int
 	}{
-		{name: "Reverse a list of size 1", list: getFilledList(values[:1]), expectedList: getFilledList(values[:1])},
-		{name: "Reverse a list of size 2", list: getFilledList(values[:2]), expectedList: getFilledList([]int{20, 10})},
-		{name: "Reverse a list of size 3", list: getFilledList(values), expectedList: getFilledList([]int{30, 20, 10})},
+		{
+			name:           "Reverse a list of size 1",
+			list:           getFilledList([]int{10}),
+			expectedValues: []int{10},
+		},
+		{
+			name:           "Reverse a list of size 2",
+			list:           getFilledList([]int{10, 20}),
+			expectedValues: []int{20, 10},
+		},
+		{
+			name:           "Reverse a list of size 3",
+			list:           getFilledList([]int{10, 20, 30}),
+			expectedValues: []int{30, 20, 10},
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			tc.list.reverse()
-			assertValues(t, getValuesFromList(tc.expectedList), getValuesFromList(tc.list))
+			assertValues(t, tc.expectedValues, getValuesFromList(tc.list))
 			assertPointer(t, tc.list.head, tc.list.tail.next)
 		})
 	}
@@ -133,16 +260,16 @@ func getFilledList(values []int) list {
 func TestGetFilledList(t *testing.T) {
 	testCases := []struct {
 		size           uint
-		payload        []int
+		values         []int
 		expectedValues []int
 	}{
-		{size: 1, payload: values[:1], expectedValues: []int{10}},
-		{size: 2, payload: values[:2], expectedValues: []int{10, 20}},
-		{size: 3, payload: values, expectedValues: values},
+		{size: 1, values: []int{10}, expectedValues: []int{10}},
+		{size: 2, values: []int{10, 20}, expectedValues: []int{10, 20}},
+		{size: 3, values: []int{10, 20, 30}, expectedValues: []int{10, 20, 30}},
 	}
 	for i, tc := range testCases {
 		t.Run(fmt.Sprintf("Test case number: %d", i+1), func(t *testing.T) {
-			l := getFilledList(tc.payload)
+			l := getFilledList(tc.values)
 			assertSize(t, tc.size, l.size)
 			assertValues(t, tc.expectedValues, getValuesFromList(l))
 			assertPointer(t, l.head, l.tail.next)
