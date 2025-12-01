7e5b7ed9d424264b8b752eee098fee804fab1522
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-12-01 22:02:13 +0200

    test: rewrite a unit test that covers the search method

--- linked_lists/singly/circular/circular_test.go
+++ linked_lists/singly/circular/circular_test.go
@@ -217,56 +217,84 @@ func TestDelete(t *testing.T) {
 }
 
 func TestSearch(t *testing.T) {
+	type expected struct {
+		index  uint
+		result bool
+	}
 	testCases := []struct {
 		name string
 		list
-		target         int
-		expectedIndex  uint
-		expectedResult bool
+		target int
+		expected
 	}{
 		{
-			name:           "Attempt to find a value in an empty list",
-			list:           list{},
-			target:         50,
-			expectedIndex:  0,
-			expectedResult: false,
+			name:     "Search in an empty list but a value doesn't exist",
+			list:     list{},
+			target:   10,
+			expected: expected{0, false},
 		},
 		{
-			name:           "Attempt to find a value in a filled list but a value doesn't exist",
-			target:         50,
-			list:           getFilledList([]int{10, 20, 30}),
-			expectedIndex:  0,
-			expectedResult: false,
+			name:     "Search in a list of size 1 but a value doesn't exist",
+			list:     getFilledList([]int{10}),
+			target:   20,
+			expected: expected{0, false},
 		},
 		{
-			name:           "Find the value that equals 10",
-			list:           getFilledList([]int{10, 20, 30}),
-			target:         10,
-			expectedIndex:  0,
-			expectedResult: true,
+			name:     "Search in a list of size 1 at the 0 index",
+			list:     getFilledList([]int{10}),
+			target:   10,
+			expected: expected{0, true},
 		},
 		{
-			name:           "Find the value that equals 20",
-			list:           getFilledList([]int{10, 20, 30}),
-			target:         20,
-			expectedIndex:  1,
-			expectedResult: true,
+			name:     "Search in a list of size 2 but a value doesn't exist",
+			list:     getFilledList([]int{10, 20}),
+			target:   30,
+			expected: expected{0, false},
 		},
 		{
-			name:           "Find the value that equals 30",
-			list:           getFilledList([]int{10, 20, 30}),
-			target:         30,
-			expectedIndex:  2,
-			expectedResult: true,
+			name:     "Search in a list of size 2 at the 0 index",
+			list:     getFilledList([]int{10, 20}),
+			target:   10,
+			expected: expected{0, true},
+		},
+		{
+			name:     "Search in a list of size 2 at the 1 index",
+			list:     getFilledList([]int{10, 20}),
+			target:   20,
+			expected: expected{1, true},
+		},
+		{
+			name:     "Search in a list of size 3 but a value doesn't exist",
+			list:     getFilledList([]int{10, 20, 30}),
+			target:   40,
+			expected: expected{0, false},
+		},
+		{
+			name:     "Search in a list of size 3 at the 0 index",
+			list:     getFilledList([]int{10, 20, 30}),
+			target:   10,
+			expected: expected{0, true},
+		},
+		{
+			name:     "Search in a list of size 3 at the 1 index",
+			list:     getFilledList([]int{10, 20, 30}),
+			target:   20,
+			expected: expected{1, true},
+		},
+		{
+			name:     "Search in a list of size 3 at the 2 index",
+			list:     getFilledList([]int{10, 20, 30}),
+			target:   30,
+			expected: expected{2, true},
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			idx, ok := tc.list.search(tc.target)
-			if want, got := tc.expectedIndex, idx; want != got {
+			if want, got := tc.expected.index, idx; want != got {
 				t.Fatalf("Expected index: %d got: %d", want, got)
 			}
-			if want, got := tc.expectedResult, ok; want != got {
+			if want, got := tc.expected.result, ok; want != got {
 				t.Errorf("Expected result: %t got: %t", want, got)
 			}
 		})
