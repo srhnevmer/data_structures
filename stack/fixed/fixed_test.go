3b412db61313697f767a20064dd3766129a757e7
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-12-09 22:24:36 +0200

    test: replace repeated if statements on func call

--- stack/fixed/fixed_test.go
+++ stack/fixed/fixed_test.go
@@ -72,48 +72,44 @@ func TestPush(t *testing.T) {
 			name:     "Add a value in an empty stack",
 			stack:    initStack(),
 			value:    10,
-			expected: expected{0, [5]int{10, 0, 0, 0, 0}},
+			expected: expected{0, [max]int{10}},
 		},
 		{
 			name:     "Add a value in a stack of size 1",
 			stack:    getFilledStack([]int{10}),
 			value:    20,
-			expected: expected{1, [5]int{10, 20, 0, 0, 0}},
+			expected: expected{1, [max]int{10, 20}},
 		},
 		{
 			name:     "Add a value in a stack of size 2",
 			stack:    getFilledStack([]int{10, 20}),
 			value:    30,
-			expected: expected{2, [5]int{10, 20, 30, 0, 0}},
+			expected: expected{2, [max]int{10, 20, 30}},
 		},
 		{
 			name:     "Add a value in a stack of size 3",
 			stack:    getFilledStack([]int{10, 20, 30}),
 			value:    40,
-			expected: expected{3, [5]int{10, 20, 30, 40, 0}},
+			expected: expected{3, [max]int{10, 20, 30, 40}},
 		},
 		{
 			name:     "Add a value in a stack of size 4",
 			stack:    getFilledStack([]int{10, 20, 30, 40}),
 			value:    50,
-			expected: expected{4, [5]int{10, 20, 30, 40, 50}},
+			expected: expected{4, [max]int{10, 20, 30, 40, 50}},
 		},
 		{
 			name:     "Attempt to add a value into a filled stack",
 			stack:    getFilledStack([]int{10, 20, 30, 40, 50}),
 			value:    60,
-			expected: expected{4, [5]int{10, 20, 30, 40, 50}},
+			expected: expected{4, [max]int{10, 20, 30, 40, 50}},
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			tc.stack.push(tc.value)
-			if want, got := tc.expected.size, tc.stack.size; want != got {
-				t.Fatalf("Expected size: %d got: %d", want, got)
-			}
-			if want, got := tc.expected.container, tc.stack.container; want != got {
-				t.Errorf("Expected result: %v got: %v", want, got)
-			}
+			assertSize(t, tc.expected.size, tc.stack.size)
+			assertContainer(t, tc.expected.container, tc.stack.container)
 		})
 	}
 }
