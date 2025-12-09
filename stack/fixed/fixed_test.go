c7ff2520b45de778cc97f365702b80f2ecac89dd
Serhii Nevmerzhytskyi <srhnevmer@proton.me>
2025-12-09 21:52:14 +0200

    test: add a unit test
    Add a unit test that covers the push method.

--- stack/fixed/fixed_test.go
+++ stack/fixed/fixed_test.go
@@ -56,6 +56,67 @@ func TestPeek(t *testing.T) {
 	}
 }
 
+func TestPush(t *testing.T) {
+	type expected struct {
+		size      int
+		container [5]int
+	}
+	testCases := []struct {
+		name string
+		stack
+		value int
+		expected
+	}{
+		{
+			name:     "Add a value in an empty stack",
+			stack:    initStack(),
+			value:    10,
+			expected: expected{0, [5]int{10, 0, 0, 0, 0}},
+		},
+		{
+			name:     "Add a value in a stack of size 1",
+			stack:    getFilledStack([]int{10}),
+			value:    20,
+			expected: expected{1, [5]int{10, 20, 0, 0, 0}},
+		},
+		{
+			name:     "Add a value in a stack of size 2",
+			stack:    getFilledStack([]int{10, 20}),
+			value:    30,
+			expected: expected{2, [5]int{10, 20, 30, 0, 0}},
+		},
+		{
+			name:     "Add a value in a stack of size 3",
+			stack:    getFilledStack([]int{10, 20, 30}),
+			value:    40,
+			expected: expected{3, [5]int{10, 20, 30, 40, 0}},
+		},
+		{
+			name:     "Add a value in a stack of size 4",
+			stack:    getFilledStack([]int{10, 20, 30, 40}),
+			value:    50,
+			expected: expected{4, [5]int{10, 20, 30, 40, 50}},
+		},
+		{
+			name:     "Attempt to add a value into a filled stack",
+			stack:    getFilledStack([]int{10, 20, 30, 40, 50}),
+			value:    60,
+			expected: expected{4, [5]int{10, 20, 30, 40, 50}},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.stack.push(tc.value)
+			if want, got := tc.expected.size, tc.stack.size; want != got {
+				t.Fatalf("Expected size: %d got: %d", want, got)
+			}
+			if want, got := tc.expected.container, tc.stack.container; want != got {
+				t.Errorf("Expected result: %v got: %v", want, got)
+			}
+		})
+	}
+}
+
 func initStack() stack {
 	return stack{size: -1}
 }
